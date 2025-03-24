a8d45b674326ff9b75d2e7f57c3af84ed393cf36
Jo Eungyeong <joeungyeong23@gmail.com>
2025-03-24 15:13:22 +0900

    feat: 홈 화면 추가

--- app/src/main/java/com/imhungry/jjongseol/MainActivity.kt
+++ app/src/main/java/com/imhungry/jjongseol/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.imhungry.jjongseol
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.imhungry.jjongseol.ui.HomeScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContent { HomeScreen() }
     }
 }
---
+++ app/src/main/java/com/imhungry/jjongseol/ui/HomeScreen.kt
@@ -0,0 +1,39 @@
+package com.imhungry.jjongseol.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(
+) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {  }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "추가")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        content = { paddingValues ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("홈 화면")
+            }
+        }
+    )
+}
+
