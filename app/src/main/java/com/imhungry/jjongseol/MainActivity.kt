00a7ec78fac3a442f5e634932ad83f77f2b16abf
Jo Eungyeong <joeungyeong23@gmail.com>
2025-03-26 00:19:44 +0900

    feat: 로그인 화면 ui 완성

--- app/src/main/java/com/imhungry/jjongseol/MainActivity.kt
+++ app/src/main/java/com/imhungry/jjongseol/MainActivity.kt
@@ -8,11 +8,18 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.imhungry.jjongseol.ui.HomeScreen
+import com.imhungry.jjongseol.ui.LoginScreen
+import com.imhungry.jjongseol.ui.LoginScreenPreview
+import com.imhungry.jjongseol.ui.theme.AppTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { HomeScreen() }
+        setContent {
+            AppTheme {
+                LoginScreen()
+            }
+        }
     }
 }
