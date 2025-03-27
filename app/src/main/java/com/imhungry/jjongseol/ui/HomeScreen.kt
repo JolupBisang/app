5e61188ddaa85a13326ff4fadf2efc114898f7aa
JeongYuJin <wjddbwls1230@naver.com>
2025-03-27 15:49:12 +0900

    feat: 회의 생성 화면(제목, 참석자, 일시, 시간 까지)

--- app/src/main/java/com/imhungry/jjongseol/ui/HomeScreen.kt
+++ app/src/main/java/com/imhungry/jjongseol/ui/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.imhungry.jjongseol.ui
 
+import android.content.Intent
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,14 +15,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 
 @Composable
 fun HomeScreen(
 ) {
+    val context = LocalContext.current
+
     Scaffold(
         floatingActionButton = {
-            FloatingActionButton(onClick = {  }) {
+            FloatingActionButton(onClick = { 
+                Toast.makeText( context,"test", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(context, CreateNewMeetingActivity::class.java)
+                context.startActivity(intent)
+            }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "추가")
             }
         },
