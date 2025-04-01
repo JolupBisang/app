1b7c4adfedf122e3073096a306fe51d9d8abe971
JeongYuJin <wjddbwls1230@naver.com>
2025-04-01 22:56:32 +0900

    feat: 버튼용 파란색 추가

--- app/src/main/java/com/imhungry/jjongseol/ui/theme/Color.kt
+++ app/src/main/java/com/imhungry/jjongseol/ui/theme/Color.kt
@@ -9,3 +9,5 @@ val md_theme_light_outline = Color(0xFF747775)
 val md_theme_dark_background = Color(0xFF131314)
 val md_theme_dark_onBackground = Color(0xFFE3E3E3)
 val md_theme_dark_outline = Color(0xFF8E918F)
+
+val md_theme_button_color_blue = Color(0xFF007AFF)
