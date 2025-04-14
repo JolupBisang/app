948ab5a9ec952f52fc8205b4af868c5bec8ef0c9
JeongYuJin <wjddbwls1230@naver.com>
2025-04-14 14:36:44 +0900

    feat: 회의 시간 선택 방식 변경(스크롤)

--- app/src/main/java/com/imhungry/jjongseol/ui/newmeeting/dateandtime/TimeSettingScreen.kt
+++ app/src/main/java/com/imhungry/jjongseol/ui/newmeeting/dateandtime/TimeSettingScreen.kt
@@ -1,176 +1,255 @@
 package com.imhungry.jjongseol.ui.newmeeting.dateandtime
 
-import android.app.TimePickerDialog
+import android.widget.NumberPicker
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.TextFieldDefaults
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.util.Calendar
-
+import androidx.compose.ui.viewinterop.AndroidView
+import java.util.*
 
 @Composable
 fun TimeDurationPicker() {
-    val context = LocalContext.current
     var startTime by remember { mutableStateOf("HH:MM") }
     var endTime by remember { mutableStateOf("HH:MM") }
     var durationInMinutes by remember { mutableStateOf("") }
-
-    fun calculateEndTime(start: String, duration: Int) {
-        val (hour, minute) = start.split(":").map { it.toInt() }
-        val calendar = Calendar.getInstance().apply {
-            set(Calendar.HOUR_OF_DAY, hour)
-            set(Calendar.MINUTE, minute)
-            add(Calendar.MINUTE, duration)
-        }
-        endTime = String.format("%02d:%02d", calendar.get(Calendar.HOUR_OF_DAY), calendar.get(
-            Calendar.MINUTE))
-    }
-
-    fun calculateDuration(startTime: String, endTime: String) {
-        if (startTime.isNotEmpty() && endTime.isNotEmpty()) {
-            val start = Calendar.getInstance().apply {
-                val (startHour, startMinute) = startTime.split(":").map { it.toInt() }
-                set(Calendar.HOUR_OF_DAY, startHour)
-                set(Calendar.MINUTE, startMinute)
-            }
-            val end = Calendar.getInstance().apply {
-                val (endHour, endMinute) = endTime.split(":").map { it.toInt() }
-                set(Calendar.HOUR_OF_DAY, endHour)
-                set(Calendar.MINUTE, endMinute)
-            }
-            if (end.before(start)) {
-                end.add(Calendar.DATE, 1)
-            }
-            val duration = (end.timeInMillis - start.timeInMillis) / (60 * 1000)
-            durationInMinutes = duration.toString()
-        }
-    }
-
-    fun pickTime(isStartTime: Boolean, updateTime: (String) -> Unit) {
-        val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
-            val formattedTime = String.format("%02d:%02d", hour, minute)
-            updateTime(formattedTime)
-            if (isStartTime) {
-                startTime = formattedTime
-            } else {
-                endTime = formattedTime
-            }
-            if (startTime.isNotEmpty() && endTime.isNotEmpty() && startTime != "HH:MM" && endTime != "HH:MM") {
-                calculateDuration(startTime, endTime)
-            }
-        }
-
-        val currentTime = Calendar.getInstance()
-        val (hour, minute) = when {
-            isStartTime && startTime.matches(Regex("\\d{2}:\\d{2}")) -> startTime.split(":").map { it.toInt() }
-            !isStartTime && endTime.matches(Regex("\\d{2}:\\d{2}")) -> endTime.split(":").map { it.toInt() }
-            else -> listOf(currentTime.get(Calendar.HOUR_OF_DAY), currentTime.get(Calendar.MINUTE))
-        }
-
-        TimePickerDialog(context, timeSetListener, hour, minute, true).show()
-    }
+    var showStartTimePicker by remember { mutableStateOf(false) }
+    var showEndTimePicker by remember { mutableStateOf(false) }
 
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
+        modifier = Modifier.fillMaxWidth().padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Button(modifier = Modifier
-                .height(60.dp),
-                onClick = { pickTime(true, { startTime = it }) },
-                shape = RoundedCornerShape(15.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.White
-                ),
-                border = BorderStroke(1.dp, Color.Gray)
-            ) {
-                Text(text = startTime)
+            Box(modifier=Modifier.weight(10f)) {
+                TimePickerButton("시작 시간", startTime,Modifier ) {
+                    showStartTimePicker = true
+                }
             }
-            Spacer(Modifier.width(5.dp))
-            Text(" ~ ",
-                style = TextStyle(
-                    fontSize = 20.sp
-                )
-            )
-            Spacer(Modifier.width(5.dp))
-            Button(modifier = Modifier
-                .height(60.dp),
-                onClick = { pickTime(false, { endTime = it }) },
-                shape = RoundedCornerShape(15.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.White
-                ),
-                border = BorderStroke(1.dp, Color.Gray),
-            ) {
-                Text(text = endTime)
+            Spacer(Modifier.weight(1f))
+            Text("~", style = MaterialTheme.typography.h6)
+            Spacer(Modifier.weight(1f))
+            Box(modifier=Modifier.weight(10f)) {
+                TimePickerButton("종료 시간", endTime, Modifier) {
+                    showEndTimePicker = true
+                }
             }
-            Spacer(Modifier.width(10.dp))
+            Spacer(Modifier.weight(1f))
+            Row(verticalAlignment = Alignment.CenterVertically, modifier=Modifier.weight(10f)){
+                Box(modifier=Modifier.weight(4f)) {
+                    DurationInput(durationInMinutes, onDurationChange = { newValue ->
+                        durationInMinutes = newValue
+                        if (startTime != "HH:MM" && newValue.isNotEmpty()) {
+                            calculateEndTime(startTime, newValue.toInt()) { calculatedEndTime ->
+                                endTime = calculatedEndTime
+                            }
+                        }
+                    })
+                }
+                Spacer(Modifier.width(10.dp))
+                Text("분", style = TextStyle(fontSize = 15.sp), modifier=Modifier.weight(1f))
+            }
+        }
 
-            Spacer(Modifier.width(10.dp))
-            OutlinedTextField(
-                value = durationInMinutes,
-                onValueChange = { newValue ->
-                    val filtered = newValue.filter { it.isDigit() }
-                    if (filtered.isNotEmpty()) {
-                        val minutes = filtered.toInt()
-                        if (minutes <= 14400) {
-                            durationInMinutes = filtered
-                        } else {
-                            durationInMinutes = "14400"
+        if (showStartTimePicker) {
+            CustomTimePickerDialog(
+                initialTime = startTime,
+                onConfirm = { selectedTime ->
+                    startTime = selectedTime
+                    showStartTimePicker = false
+                    if (endTime != "HH:MM") {
+                        updateDurationFromTimes(startTime, endTime) {
+                            durationInMinutes = it.toString()
                         }
-                        if (startTime.matches(Regex("\\d{2}:\\d{2}"))) {
-                            calculateEndTime(startTime, durationInMinutes.toInt())
-                        }
-                    } else {
-                        durationInMinutes = ""
                     }
                 },
-                singleLine = true,
-                modifier = Modifier
-                    .width(80.dp)
-                    .height(60.dp)
-                    .border(1.dp, Color.Gray, RoundedCornerShape(15.dp)),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    textColor = Color.Black,
-                    cursorColor = Color.Black,
-                    backgroundColor = Color.White,
-                    focusedBorderColor = Color.Transparent,
-                    unfocusedBorderColor = Color.Transparent
-                )
-            )
-            Spacer(Modifier.width(10.dp))
-            Text("분",
-                style = TextStyle(
-                    fontSize = 15.sp
-                )
+                onDismiss = {
+                    showStartTimePicker = false
+                }
             )
         }
 
+        if (showEndTimePicker) {
+            CustomTimePickerDialog(
+                initialTime = endTime,
+                onConfirm = { selectedTime ->
+                    endTime = selectedTime
+                    showEndTimePicker = false
+                    if (startTime != "HH:MM") {
+                        updateDurationFromTimes(startTime, endTime) {
+                            durationInMinutes = it.toString()
+                        }
+                    }
+                },
+                onDismiss = {
+                    showEndTimePicker = false
+                }
+            )
+        }
+
+    }
+}
+
+fun calculateEndTime(start: String, duration: Int, onResult: (String) -> Unit) {
+    val (hour, minute) = start.split(":").map { it.toInt() }
+    val calendar = Calendar.getInstance().apply {
+        set(Calendar.HOUR_OF_DAY, hour)
+        set(Calendar.MINUTE, minute)
+        add(Calendar.MINUTE, duration)
+    }
+    onResult(String.format("%02d:%02d", calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE)))
+}
+
+fun updateDurationFromTimes(start: String, end: String, onResult: (Int) -> Unit) {
+    val startCalendar = Calendar.getInstance().apply {
+        val (hour, minute) = start.split(":").map { it.toInt() }
+        set(Calendar.HOUR_OF_DAY, hour)
+        set(Calendar.MINUTE, minute)
+    }
+
+    val endCalendar = Calendar.getInstance().apply {
+        val (hour, minute) = end.split(":").map { it.toInt() }
+        set(Calendar.HOUR_OF_DAY, hour)
+        set(Calendar.MINUTE, minute)
+    }
+
+    if (endCalendar.before(startCalendar)) {
+        endCalendar.add(Calendar.DATE, 1)  //다음 날로 설정하기
+    }
+
+    val duration = ((endCalendar.timeInMillis - startCalendar.timeInMillis) / 60000).toInt()
+    onResult(duration)
+}
+
+
+@Composable
+fun TimePickerButton(label: String, time: String, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .height(60.dp)
+            .fillMaxWidth()
+            .widthIn(min = 100.dp),
+        shape = RoundedCornerShape(15.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+        border = BorderStroke(1.dp, Color.Gray)
+    ) {
+        Text(time, fontSize = 16.sp)
+    }
+}
+
+@Composable
+fun DurationInput(value: String, modifier: Modifier = Modifier, onDurationChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = {
+            try {
+                var numericValue = it.toInt()
+                if (numericValue > 1440) {
+                    numericValue = 1440
+                }
+                onDurationChange(numericValue.toString())
+            } catch (e: NumberFormatException) {
+                if (it.isBlank()) {
+                    onDurationChange("0")
+                }
+            }
+        },
+        label = { Text("") },
+        singleLine = true,
+        modifier = modifier
+            .height(60.dp)
+            .border(1.dp, Color.Gray, RoundedCornerShape(15.dp))
+            .fillMaxWidth(),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            textColor = Color.Black,
+            cursorColor = Color.Black,
+            backgroundColor = Color.White,
+            focusedBorderColor = Color.Transparent,
+            unfocusedBorderColor = Color.Transparent
+        ),
+        textStyle = TextStyle(
+            fontSize = 15.sp,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
+    )
+}
+
+
+@Composable
+fun CustomTimePickerDialog(
+    initialTime: String,
+    onConfirm: (String) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val validInitialTime = if (initialTime.matches(Regex("\\d{2}:\\d{2}"))) initialTime else "12:00"
+    var selectedHour by remember { mutableStateOf(validInitialTime.split(":").first().toInt()) }
+    var selectedMinute by remember { mutableStateOf(validInitialTime.split(":").last().toInt()) }
+    var showDialog by remember { mutableStateOf(true) }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = {
+                showDialog = false
+                onDismiss()},
+            confirmButton = {
+                TextButton(onClick = {
+                    val formattedTime = String.format("%02d:%02d", selectedHour, selectedMinute)
+                    onConfirm(formattedTime)
+                    showDialog = false
+                }) {
+                    Text("확인")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    showDialog = false
+                    onDismiss() }
+                )
+                {
+                    Text("취소")
+                }
+            },
+            text = {
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center) {
+                    AndroidView(
+                        factory = { context ->
+                            NumberPicker(context).apply {
+                                minValue = 0
+                                maxValue = 23
+                                value = selectedHour
+                                setOnValueChangedListener { _, _, newVal ->
+                                    selectedHour = newVal
+                                }
+                            }
+                        }
+                    )
+                    Spacer(Modifier.width(20.dp))
+                    AndroidView(
+                        factory = { context ->
+                            NumberPicker(context).apply {
+                                minValue = 0
+                                maxValue = 59
+                                value = selectedMinute
+                                setOnValueChangedListener { _, _, newVal ->
+                                    selectedMinute = newVal
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        )
     }
 }
