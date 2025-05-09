999663b7a8914db8c618cd6350f97c7a37ec0f34
Jo Eungyeong <joeungyeong23@gmail.com>
2025-05-09 10:21:35 +0900

    refactor: 로그인 화면 로고, 배경 색상 변경

--- app/src/main/java/com/imhungry/jjongseol/ui/login/LoginScreen.kt
+++ app/src/main/java/com/imhungry/jjongseol/ui/login/LoginScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -52,14 +53,14 @@ fun LoginScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
+            .background(Color(0xFFF1EBE0)),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.weight(1.6f))
 
         Image(
-            painter = painterResource(id = R.drawable.login_image),
-            contentDescription = "login image",
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "logo",
             modifier = Modifier.size(200.dp)
         )
 
