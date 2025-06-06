26b6a9d7de7ff6eebdc0a912a896cc770fb47cd2
JeongYuJin <wjddbwls1230@naver.com>
2025-06-06 21:36:16 +0900

    feat: 목소리 인식 화면에 gif 추가

--- app/src/main/java/com/imhungry/jjongseol/ui/learningvoice/LearningVoiceFirstScreen.kt
+++ app/src/main/java/com/imhungry/jjongseol/ui/learningvoice/LearningVoiceFirstScreen.kt
@@ -11,18 +11,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.request.ImageRequest
 import com.imhungry.jjongseol.R
 import com.imhungry.jjongseol.ui.theme.SetNavigationBarColor
 import com.imhungry.jjongseol.ui.theme.UserGreen1
 import com.imhungry.jjongseol.ui.theme.blackColor
+import com.imhungry.jjongseol.ui.theme.greenTitle1
+import com.imhungry.jjongseol.ui.theme.greenTitle2
+import com.imhungry.jjongseol.ui.theme.greenTitle3
+import com.imhungry.jjongseol.ui.theme.greenTitle4
+import com.imhungry.jjongseol.ui.theme.greenTitle5
 import com.imhungry.jjongseol.ui.theme.whiteColor
 
 @Composable
@@ -34,12 +44,57 @@ fun LearningVoiceFirstScreen(navController: NavController) {
             .background(blackColor)
             .navigationBarsPadding()
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.learningvoicefirstscreen),
-            contentDescription = "Learning Voice First Screen Background",
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
-        )
+        val gradientColors = listOf(greenTitle1, greenTitle2, greenTitle3, greenTitle4, greenTitle5)
+
+        Column(modifier = Modifier.fillMaxWidth().padding(top = 130.dp),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(R.drawable.gif)
+                        .decoderFactory(GifDecoder.Factory())
+                        .build()
+                ),
+                contentDescription = "목소리 인식 시작 gif",
+                modifier = Modifier.size(140.dp).fillMaxWidth()
+            )
+            Row (verticalAlignment = Alignment.CenterVertically){
+                Text(
+                    text = "당신의 목소리",
+                    style = TextStyle(
+                        brush = Brush.linearGradient(
+                            colors = gradientColors
+                        ),
+                        fontSize = 30.sp, fontWeight = FontWeight.Bold
+                    )
+                )
+                Text(
+                    text = "를",
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Text(
+                text = "알려주세요",
+                color = Color.White,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    modifier = Modifier.padding(top = 20.dp),
+                    text = "이 설정을 통해 회의 중 발화자를 구분할 수 있습니다.",
+                    color = Color.White,
+                    fontSize = 13.sp,
+                )
+                Text(
+                    text = "조용한 환경에서 평소처럼 말해주시길 바랍니다.",
+                    color = Color.White,
+                    fontSize = 13.sp,
+                )
+            }
+        }
 
         Column(
             modifier = Modifier
