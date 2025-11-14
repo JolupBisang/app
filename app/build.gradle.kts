405988c792f6e396eda8acb289894a54c0e34905
Jo Eungyeong <joeungyeong23@gmail.com>
2025-11-15 03:52:19 +0900

    fix: libs.androidx.foundation.layout 의존성 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,6 @@ dependencies {
     
     // Dependency Injection - Hilt
     implementation("com.google.dagger:hilt-android:2.48")
-    implementation(libs.androidx.foundation.layout)
     ksp("com.google.dagger:hilt-android-compiler:2.48")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     
