dff79c5a118b2918f299013cfcd2e6dfbc50adf2
Jo Eungyeong <joeungyeong23@gmail.com>
2025-11-18 05:27:56 +0900

    refactor: 디자인 수정

--- app/src/main/java/com/imhungry/sillok/presentation/screen/login/LoginScreen.kt
+++ app/src/main/java/com/imhungry/sillok/presentation/screen/login/LoginScreen.kt
@@ -3,10 +3,12 @@ package com.imhungry.sillok.presentation.screen.login
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -16,20 +18,27 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.request.ImageRequest
 import com.imhungry.sillok.R
+import com.imhungry.sillok.presentation.state.login.LoginState
 import com.imhungry.sillok.presentation.viewmodel.login.LoginViewModel
 import com.imhungry.sillok.ui.components.BasicBox
 import com.imhungry.sillok.ui.components.ExitDialog
-import com.imhungry.sillok.ui.theme.Monda
-import com.imhungry.sillok.ui.theme.beige
+import com.imhungry.sillok.ui.theme.SillokTheme
+import com.imhungry.sillok.ui.theme.secondaryButton
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 @Composable
 fun LoginScreen(
@@ -71,9 +80,9 @@ fun LoginScreen(
     )
 
     BasicBox(
-        statusBarColor = beige,
-        navigationBarColor = beige,
-        backgroundColor = beige,
+        statusBarColor = Color(0xFFFEFCFE),
+        navigationBarColor = Color(0xFFFEFCFE),
+        backgroundColor = Color(0xFFFEFCFE),
         isLoading = state.isLoading
     ) {
         Column(
@@ -82,16 +91,14 @@ fun LoginScreen(
             verticalArrangement = Arrangement.Center
         ) {
             Image(
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = "Logo",
-                modifier = Modifier.size(100.dp)
-            )
-            Text(
-                text = "sealog",
-                fontFamily = Monda,
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Medium,
-                textAlign = TextAlign.Center
+                painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(R.drawable.splash)
+                        .decoderFactory(GifDecoder.Factory())
+                        .build()
+                ),
+                contentDescription = "splash gif",
+                modifier = Modifier.size(230.dp)
             )
             Spacer(modifier = Modifier.height(60.dp))
 
@@ -107,6 +114,55 @@ fun LoginScreen(
                 onClick = { loginViewModel.launchGoogleOAuth(context) },
                 isLoading = state.isLoading
             )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LoginScreenPreview() {
+    SillokTheme {
+        BasicBox(
+            statusBarColor = secondaryButton,
+            navigationBarColor = secondaryButton,
+            backgroundColor = secondaryButton,
+            isLoading = false
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "Logo",
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.sealog),
+                        contentDescription = "Sealog",
+                        modifier = Modifier.size(132.dp)
+                            .padding(top = 100.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(60.dp))
+
+                Text(
+                    text = "로그인하고 바로 시작하세요",
+                    style = MaterialTheme.typography.titleSmall,
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                GoogleLoginButton(
+                    onClick = {  },
+                    isLoading = false
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/imhungry/sillok/presentation/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/imhungry/sillok/presentation/screen/splash/SplashScreen.kt
@@ -2,25 +2,45 @@ package com.imhungry.sillok.presentation.screen.splash
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.request.ImageRequest
 import com.imhungry.sillok.R
+import com.imhungry.sillok.presentation.screen.login.GoogleLoginButton
 import com.imhungry.sillok.presentation.viewmodel.splash.SplashViewModel
+import com.imhungry.sillok.ui.components.BasicBox
 import com.imhungry.sillok.ui.components.SystemBars
+import com.imhungry.sillok.ui.theme.SillokTheme
 import com.imhungry.sillok.ui.theme.beige
+import com.imhungry.sillok.ui.theme.secondaryButton
 
 @Composable
 fun SplashScreen(
@@ -44,14 +64,14 @@ fun SplashScreen(
     }
 
     SystemBars(
-        statusBarColor = beige,
-        navigationBarColor = beige,
+        statusBarColor = Color(0xFFFEFCFE),
+        navigationBarColor = Color(0xFFFEFCFE),
     )
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(beige)
+            .background(Color(0xFFFEFCFE))
             .windowInsetsPadding(WindowInsets.systemBars)
     ) {
         Box(
@@ -60,11 +80,45 @@ fun SplashScreen(
             contentAlignment = Alignment.Center
         ) {
             Image(
-                painter = painterResource(id = R.drawable.splash),
-                contentDescription = "splash",
-                modifier = Modifier.fillMaxWidth(),
-                contentScale = ContentScale.Fit
+                painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(R.drawable.splash)
+                        .decoderFactory(GifDecoder.Factory())
+                        .build()
+                ),
+                contentDescription = "splash gif",
+                modifier = Modifier.size(230.dp)
             )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SplashScreenPreview() {
+    SillokTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFFEFCFE))
+                .windowInsetsPadding(WindowInsets.systemBars)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(R.drawable.splash)
+                            .decoderFactory(GifDecoder.Factory())
+                            .build()
+                    ),
+                    contentDescription = "splash gif",
+                    modifier = Modifier.size(132.dp)
+                )
+            }
         }
     }
 }
